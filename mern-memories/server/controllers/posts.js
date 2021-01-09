3d569ec189522701000884f78d64d5191aab0800
AlexeySvyridov <alexey.svyridov@vitalerter.com>
2021-01-09 12:51:24 +0200

    add .

---
+++ mern-memories/server/controllers/posts.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await PostMessage.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({message: error.message})
+  }
+}; 
+
+export const createPost = async(req, res) => {
+  const post = await req.body;
+  const newPost = new PostMessage(post)
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({message:error.message})    
+  }
+};
+
+export const updatePost = async(req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if(!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('No post with that id');
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+  res.json(updatedPost)
+};
