4fb5547a77e2cd8ece2bd2ca9aca8c7b3f047ab0
ALEKSEY <33163048+alexeysvyridov@users.noreply.github.com>
2021-01-09 22:12:37 +0200

    Add files via upload

--- app/server/index.js
+++ app/server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-const app = express()
+import dotenv from 'dotenv';
 
+const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
@@ -13,7 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://alexeysvyridov123:alexeysvyridov123@cluster0.mfy5g.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req,res) => {
+  res.send('Hello to Memories API')
+})
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
